36b979cb85f61e93ae58233e8faca37b8943f95e
Jan Stępień <jan@stepien.cc>
2017-05-09 22:56:36 +0200

    Get four out of six tests passing
    That's how far Helen and I got 😄

--- labyrinth/src/labyrinth/solution.clj
+++ labyrinth/src/labyrinth/solution.clj
@@ -1,3 +1,46 @@
 (ns labyrinth.solution)
 
-(defn solve [labyrinth])
+(defn start-pos [lab]
+  (if (->> (apply concat lab)
+           (filter #{1})
+           (count)
+           (= 1))
+    [0 0]))
+
+(defn can-go? [cell]
+  (not (or (= cell :W)
+           (= cell 1)
+           (nil? cell))))
+
+(defn neighbours [lab pos]
+  (let [directions [[0 1]
+                    [1 0]
+                    [0 -1]
+                    [-1 0]]
+        [posx posy] pos]
+    (->> (for [[dx dy] directions]
+           (let [x (+ dx posx)
+                 y (+ dy posy)
+                 what-is-there (get-in lab [x y])]
+             [x y what-is-there]))
+         (filter (fn [[x y what-is-there]]
+                   (can-go? what-is-there)))
+         (map butlast))))
+
+(def choose-from
+  first)
+
+(defn solve-from [lab pos]
+  (let [next-pos (choose-from (neighbours lab pos))
+        next-lab (if next-pos
+                   (assoc-in lab next-pos 1))]
+    (cond
+      (= :Exit (get-in lab next-pos)) next-lab
+      (nil? next-pos) ["unable to exit"]
+      :else (solve-from next-lab next-pos))))
+
+(defn solve [lab]
+  (let [pos (start-pos lab)]
+    (if (nil? pos)
+      lab
+      (solve-from lab pos))))
